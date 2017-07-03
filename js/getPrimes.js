c93821e169169889c47f692b8db7988ae77dddda
THEozmic <michaelozoemena2@gmail.com>
2017-07-03 17:03:42 +0100

    Add decriptive error messages in console

--- js/getPrimes.js
+++ js/getPrimes.js
@@ -1,13 +1,17 @@
 function getPrimes (number) {
+    
     if (typeof number == "undefined") {
+        console.log("getPrimes requires a parameter of positive integer, greater than 1");
         return false;
     }
 
     if (isNaN(number)) {
+        console.log("getPrimes requires an integer as a parameter");
         return false;
     }
 
     if (number < 2) {
+        console.log("getPrimes requires an integer greater than 1 as it's only parameter");
         return false;
     }
 
@@ -15,7 +19,7 @@ function getPrimes (number) {
     let nonPrimes = [];
 
     for (let i = 2; i <= number; ++i) {
-      if (nonPrimes[i] == undefined) { // not in nonPrimes then...
+      if (nonPrimes[i] === undefined) { // not in nonPrimes then...
           // it's a prime number
           primes.push(i);
           // it seems that multiples of 2 aren't prime numbers, so...
